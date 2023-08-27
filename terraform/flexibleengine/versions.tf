d544a323d0098d0175a07101bb5717f97032b303
Ahmed ElDakhli <ahmedeldakhli@gmail.com>
2023-08-27 17:46:27 +0000

    Update Environment Variables To test terrafotm run

--- terraform/flexibleengine/versions.tf
+++ terraform/flexibleengine/versions.tf
@@ -9,6 +9,21 @@ terraform {
     }
   }
 }
+# Access Environmental Variables
+variable "OS_ACCESS_KEY" {}
+variable "OS_SECRET_KEY" {}
+variable "OS_AUTH_URL" {}
+variable "OS_DOMAIN_NAME" {}
+variable "OS_PROJECT_NAME" {}
+
+#Configure the FlexibleEngine Provider
+provider "flexibleengine" {
+ domain_name = var.OS_DOMAIN_NAME
+ access_key   = var.OS_ACCESS_KEY
+ secret_key    = var.OS_SECRET_KEY
+ region      = var.OS_REGION_NAME
+ tenant_name = var.OS_PROJECT_NAME
+}
 
 # terraform {
 #   required_version = ">= 0.13"
